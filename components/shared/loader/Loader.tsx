92606dfd95548ef8f164179cf000133e797338cd
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-07-14 17:39:16 +0300

    feat: Added shared loader

---
+++ components/shared/loader/Loader.tsx
@@ -0,0 +1,7 @@
+const Loader = () => {
+  return (
+    <div className="mx-auto aspect-[1] w-[15px] animate-loader rounded-full" />
+  );
+};
+
+export default Loader;
--- tailwind.config.js
+++ tailwind.config.js
@@ -43,11 +43,30 @@ const config = {
       },
       animation: {
         marquee: "marquee 12s linear infinite",
+        loader: "l5 1s infinite linear alternate",
       },
       keyframes: {
         marquee: {
           "0%": { transform: "translateX(0%)" },
           "100%": { transform: "translateX(-100%)" },
+        },
+        l5: {
+          "0%": {
+            boxShadow: "20px 0 #FF7300, -20px 0 #FF9D4C",
+            background: "#FF7300",
+          },
+          "33%": {
+            boxShadow: "20px 0 #FF7300, -20px 0 #FF9D4C",
+            background: "#FF9D4C",
+          },
+          "66%": {
+            boxShadow: "20px 0 #FF9D4C, -20px 0 #FF7300",
+            background: "#FF9D4C",
+          },
+          "100%": {
+            boxShadow: "20px 0 #FF9D4C, -20px 0 #FF7300",
+            background: "#FF7300",
+          },
         },
       },
 
