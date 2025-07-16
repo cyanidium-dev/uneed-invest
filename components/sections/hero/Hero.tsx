4ef99e11402b3486b8e143ee678eb3c908a57b22
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-07-16 11:25:56 +0300

    styles: Added animations in Hero section

--- components/sections/hero/Hero.tsx
+++ components/sections/hero/Hero.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 
+import AnimatedWrapper from "@/components/shared/animated/AnimatedWrapper";
 import StarInfoBox from "@/components/shared/box/StarInfoBox";
 import BaseButton from "@/components/shared/buttons/BaseButton";
 import Marquee from "@/components/shared/marquee/Marquee";
 import { cn } from "@/utils/cn";
+import { fadeInAnimation } from "@/helpers/animation";
 
 import HeroInvestment from "./HeroInvestment";
 
@@ -12,27 +14,34 @@ const Hero = () => {
     <section className="relative overflow-hidden pb-[100px] pt-[242px] xl:pt-[189px]">
       <div className="container relative z-10 flex max-w-[1280px] flex-col xl:flex-row xl:justify-between">
         <div className="max-w-[457px] max-xl:mx-auto max-xl:mb-[177px]">
-          <StarInfoBox
-            boxVariant="light"
-            text="Твоя частка в майбутньому"
-            boxClassName="max-xl:hidden mb-[47px] w-[387px]"
-          />
+          <AnimatedWrapper animation={fadeInAnimation({ x: 50 })}>
+            <StarInfoBox
+              boxVariant="light"
+              text="Твоя частка в майбутньому"
+              boxClassName="max-xl:hidden mb-[47px] w-[387px]"
+            />
+          </AnimatedWrapper>
 
-          <h1 className="mb-4 text-[32px] font-bold uppercase leading-none max-xl:h-[148px] max-xl:text-center xl:mb-9 xl:text-[52px]">
-            Інвестуйте в мережу подобової оренди житла
-          </h1>
-          <p className="mb-10 font-manrope font-light leading-[1.22] max-xl:text-center xl:mb-[70px] xl:text-[20px]">
-            Бізнес з повною автоматизацією: без ресепшену, без черг, без суєти
-          </p>
+          <AnimatedWrapper animation={fadeInAnimation({ y: -50, delay: 0.2 })}>
+            <h1 className="mb-4 text-[32px] font-bold uppercase leading-none max-xl:h-[148px] max-xl:text-center xl:mb-9 xl:text-[52px]">
+              Інвестуйте в мережу подобової оренди житла
+            </h1>
+            <p className="mb-10 font-manrope font-light leading-[1.22] max-xl:text-center xl:mb-[70px] xl:text-[20px]">
+              Бізнес з повною автоматизацією: без ресепшену, без черг, без суєти
+            </p>
+          </AnimatedWrapper>
 
-          <div className="flex flex-col gap-3 xl:flex-row xl:justify-between">
+          <AnimatedWrapper
+            animation={fadeInAnimation({ y: 50, delay: 0.4 })}
+            className="flex flex-col gap-3 xl:flex-row xl:justify-between"
+          >
             <BaseButton variant="accent" className="xl:w-[204px]">
               Стати партнером
             </BaseButton>
             <BaseButton variant="light" className="xl:w-[233px]">
               Отримати презентацію
             </BaseButton>
-          </div>
+          </AnimatedWrapper>
         </div>
 
         <Marquee
--- components/sections/hero/HeroInvestment.tsx
+++ components/sections/hero/HeroInvestment.tsx
@@ -1,5 +1,9 @@
+import AnimatedList from "@/components/shared/animated/AnimatedList";
+import AnimatedListItem from "@/components/shared/animated/AnimatedListItem";
+import AnimatedWrapper from "@/components/shared/animated/AnimatedWrapper";
 import InfoBox from "@/components/shared/box/InfoBox";
 import Circles from "@/components/shared/decor/Circles";
+import { fadeInAnimation, listVariants } from "@/helpers/animation";
 import { investmentOptions } from "@/data/investmentOptions";
 
 const HeroInvestment = () => {
@@ -7,17 +11,23 @@ const HeroInvestment = () => {
     <div className="max-xl:mx-auto max-xl:w-full max-xl:max-w-[330px] xl:w-[471px]">
       {investmentOptions.map(({ label, variant, items, decor }) => (
         <div key={label} className="mb-7 last:mb-0 xl:mb-8 xl:first:mt-[80px]">
-          <div className="mb-3 flex items-center justify-between xl:mb-5">
+          <AnimatedWrapper
+            animation={fadeInAnimation({ x: 50, delay: 0.5 })}
+            className="mb-3 flex items-center justify-between xl:mb-5"
+          >
             {decor && <Circles />}
 
             <h2 className="text-[24px] font-bold uppercase leading-none xl:text-[36px]">
               {label}
             </h2>
-          </div>
+          </AnimatedWrapper>
 
-          <ul className="flex justify-between">
+          <AnimatedList
+            animation={listVariants({ staggerChildren: 0.5 })}
+            className="flex justify-between"
+          >
             {items.map((item, i) => (
-              <li key={i}>
+              <AnimatedListItem key={i}>
                 <InfoBox
                   className={
                     "px-[10px] py-5 text-center font-montserrat text-sm font-semibold uppercase leading-[20px] text-white xl:px-[15px] xl:py-[34px]"
@@ -38,9 +48,9 @@ const HeroInvestment = () => {
                     </span>
                   )}
                 </InfoBox>
-              </li>
+              </AnimatedListItem>
             ))}
-          </ul>
+          </AnimatedList>
         </div>
       ))}
     </div>
