14f7963f48b5bdbc184d8e5c9ef79303095d248a
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-07-16 17:28:37 +0300

    fix Optimization

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,7 @@
 import dynamic from "next/dynamic";
 
 import About from "@/components/sections/about/About";
-// import Advantages from "@/components/sections/advantages/Advantages";
-// import BotCTA from "@/components/sections/botCTA/BotCTA";
-// import Business from "@/components/sections/business/Business";
-// import Calculator from "@/components/sections/calculator/Calculator";
-// import Estimate from "@/components/sections/estimate/Estimate";
-// import FAQ from "@/components/sections/faq/FAQ";
 import Hero from "@/components/sections/hero/Hero";
-
-// import Investment from "@/components/sections/investment/Investment";
-// import Locations from "@/components/sections/locations/Locations";
-// import MidCTA from "@/components/sections/midCTA/MidCTA";
-// import Occupancy from "@/components/sections/occupancy/Occupancy";
-// import Partnership from "@/components/sections/partnership/Partnership";
-// import Reviews from "@/components/sections/reviews/Reviews";
-// import Token from "@/components/sections/token/Token";
-// import TopCTA from "@/components/sections/topCTA/TopCTA";
 
 const Advantages = dynamic(
   () => import("@/components/sections/advantages/Advantages")
