f087407e700752d958c3675d311c13d9e2c1fd86
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-07-16 18:20:10 +0300

    Update Hero.tsx

--- components/sections/hero/Hero.tsx
+++ components/sections/hero/Hero.tsx
@@ -45,19 +45,36 @@ const Hero = () => {
         <HeroInvestment />
       </div>
 
-      <div className="absolute left-0 top-0 h-full w-full overflow-hidden xl:w-1/2">
-        <Image
-          src="/images/hero/hero-bg-desk.webp"
-          alt="Фонове зображення"
-          fill
-          priority
-          fetchPriority="high"
-          loading="eager"
-          sizes="(min-width: 1280px) 50vw, 100vw"
-          className="object-cover"
-        />
+      <div className="absolute left-0 top-0 h-full w-full xl:w-1/2">
+        <div className="absolute left-0 top-0 h-full w-full overflow-hidden">
+          <Image
+            src="/images/hero/hero-bg-desk.webp"
+            alt="Фонове зображення"
+            fill
+            priority
+            fetchPriority="high"
+            loading="eager"
+            sizes="(min-width: 1280px) 50vw, 100vw"
+            className="object-cover"
+          />
 
-        <div className="absolute bottom-0 z-[1] h-[936px] w-[663px] bg-white/[37%] shadow-[inset_0px_3.5px_10.5px_rgba(0,0,0,0.25)] backdrop-blur-[28.7px] [border-radius:50%/50%] max-xl:left-1/2 max-xl:-translate-x-1/2 xl:-bottom-[299px] xl:-right-[88px] xl:h-[935px] xl:w-[948px]" />
+          <div className="absolute bottom-0 z-[1] h-[936px] w-[663px] bg-white/[37%] shadow-[inset_0px_3.5px_10.5px_rgba(0,0,0,0.25)] backdrop-blur-[28.7px] [border-radius:50%/50%] max-xl:left-1/2 max-xl:-translate-x-1/2 xl:-bottom-[299px] xl:-right-[88px] xl:h-[935px] xl:w-[948px]" />
+        </div>
+
+        <div className="absolute -right-[420px] bottom-0 !block max-xl:hidden xl:h-[306px] xl:w-[799px]">
+          <Image
+            src="/images/hero/hero-decor-bot-desk.svg"
+            alt="Декоративне зображення"
+            // width={799}
+            // height={306}
+            // className="absolute -right-[500px] bottom-0 !block max-xl:hidden"
+            fill
+            sizes="799px"
+            priority
+            loading="eager"
+            fetchPriority="high"
+          />
+        </div>
       </div>
 
       <Image
@@ -77,17 +94,6 @@ const Hero = () => {
         width={378}
         height={356}
         className="absolute right-0 top-0 max-xl:hidden"
-        priority
-        loading="eager"
-        fetchPriority="high"
-      />
-
-      <Image
-        src="/images/hero/hero-decor-bot-desk.svg"
-        alt="Декоративне зображення"
-        width={799}
-        height={306}
-        className="absolute bottom-0 right-[23%] max-xl:hidden"
         priority
         loading="eager"
         fetchPriority="high"
