0318b85f2bf3fc082419aec01f7222b7f9333c5a
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-07-17 19:33:13 +0300

    styles: Added variants to FormField

--- components/shared/form/FormField.tsx
+++ components/shared/form/FormField.tsx
@@ -1,19 +1,63 @@
 "use client";
 
-import { Controller, useFormContext } from "react-hook-form";
+import {
+  Controller,
+  ControllerFieldState,
+  useFormContext,
+} from "react-hook-form";
 
 import { Input } from "@heroui/input";
 
 import { cn } from "@/utils/cn";
 
+type inputVariants = "dark" | "light";
+
 interface IFormFieldProps {
   label: string;
   name: string;
   type: string;
+  variant?: inputVariants;
 }
 
-const FormField = ({ label, name, type }: IFormFieldProps) => {
+const FormField = ({
+  label,
+  name,
+  type,
+  variant = "light",
+}: IFormFieldProps) => {
   const { control } = useFormContext();
+
+  const mergeClasses = (
+    fieldState: ControllerFieldState,
+    variant: inputVariants
+  ) => {
+    const classNames = {
+      base: "relative",
+      label: cn(
+        "font-manrope text-[12px] leading-[20px] font-light",
+        variant === "light" &&
+          "text-light group-data-[filled-within=true]:text-light",
+        variant === "dark" &&
+          "text-dark group-data-[filled-within=true]:text-dark",
+        fieldState.error && "!text-error"
+      ),
+      inputWrapper: cn(
+        "",
+        variant === "light" && "group-data-[focus=true]:border-accent-light",
+        variant === "dark" &&
+          "border-dark group-data-[focus=true]:border-accent",
+        fieldState.error &&
+          "!border-error group-data-[focus=true]:!border-error"
+      ),
+      input: cn(
+        fieldState.error && "!text-error group-data-[focus=true]:!text-error"
+      ),
+      helperWrapper: "absolute -bottom-[20px] left-0",
+      errorMessage: "text-[12px] text-error",
+    };
+
+    return classNames;
+  };
 
   return (
     <Controller
@@ -21,28 +65,12 @@ const FormField = ({ label, name, type }: IFormFieldProps) => {
       name={name}
       render={({ field, fieldState }) => (
         <Input
-          classNames={{
-            base: "relative",
-            label: cn(
-              "text-light font-manrope text-[12px] leading-[20px] font-light group-data-[filled-within=true]:text-light",
-              fieldState.error && "!text-error"
-            ),
-            inputWrapper: cn(
-              "group-data-[focus=true]:border-accent-light",
-              fieldState.error &&
-                "!border-error group-data-[focus=true]:!border-error"
-            ),
-            input: cn(
-              fieldState.error &&
-                "!text-error group-data-[focus=true]:!text-error"
-            ),
-            helperWrapper: "absolute -bottom-[20px] left-0",
-            errorMessage: "text-[12px] text-error",
-          }}
+          classNames={mergeClasses(fieldState, variant)}
           {...field}
           label={label}
           name={name}
           type={type}
+          data-variant={variant}
           variant="bordered"
           radius="full"
           value={field.value}
