8d12269f90c0c2518602eb8ed305eec7781c6390
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-07-18 13:41:15 +0300

    styles: Added animations on TopCTA

--- components/sections/topCTA/TopCTA.tsx
+++ components/sections/topCTA/TopCTA.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
 
 import TriggerFeedbackForm from "@/components/module/triggerForm/TriggerFeedbackForm";
+import AnimatedWrapper from "@/components/shared/animated/AnimatedWrapper";
 import StarInfoBox from "@/components/shared/box/StarInfoBox";
+import { fadeInAnimation } from "@/helpers/animation";
 
 const TopCTA = () => {
   return (
     <section className="bg-accent py-10 text-light xl:py-[112px]">
       <div className="container relative max-w-[500px] xl:max-w-[1280px]">
-        <div className="mb-6 max-xl:max-w-[285px] xl:mb-10 xl:ml-auto xl:w-[365px]">
+        <AnimatedWrapper
+          animation={fadeInAnimation({ x: -50 })}
+          className="mb-6 max-xl:max-w-[285px] xl:mb-10 xl:ml-auto xl:w-[365px]"
+        >
           <h2 className="mb-4 text-[24px] font-bold uppercase leading-none xl:text-[32px]">
             Станьте
             <br />
@@ -16,24 +21,36 @@ const TopCTA = () => {
           <p className="font-manrope text-[12px] leading-none xl:text-[16px]">
             Отримайте персональну консультацію та відповіді на всі питання
           </p>
-        </div>
+        </AnimatedWrapper>
 
-        <TriggerFeedbackForm />
+        <AnimatedWrapper animation={fadeInAnimation({ y: 50, delay: 0.3 })}>
+          <TriggerFeedbackForm />
+        </AnimatedWrapper>
 
         <div className="absolute -top-[112px] left-0 flex h-[610px] w-[742px] items-end max-xl:hidden">
-          <Image
-            src="/images/topCTA/topCTA-bg.webp"
-            alt="Фонове зображення"
-            fill
-            sizes="742px"
-            className="rounded-r-xl object-cover"
-          />
-          <StarInfoBox
-            text="5 апартаментів = 2300$ прибуток"
-            boxVariant="light"
-            starVariant="accent-dark"
-            boxClassName="w-[418px] relative z-[1]"
-          />
+          <AnimatedWrapper
+            animation={fadeInAnimation({ scale: 0.8, delay: 0.4 })}
+            className="absolute left-0 top-0 h-full w-[742px]"
+          >
+            <Image
+              src="/images/topCTA/topCTA-bg.webp"
+              alt="Фонове зображення"
+              fill
+              sizes="742px"
+              className="rounded-r-xl object-cover"
+            />
+          </AnimatedWrapper>
+          <AnimatedWrapper
+            animation={fadeInAnimation({ x: 50, delay: 0.7 })}
+            className="relative z-[1] w-[418px]"
+          >
+            <StarInfoBox
+              text="5 апартаментів = 2300$ прибуток"
+              boxVariant="light"
+              starVariant="accent-dark"
+              boxClassName="w-[418px] relative z-[1]"
+            />
+          </AnimatedWrapper>
         </div>
       </div>
     </section>
