a342ae787d2c5055ea9fab4ef4a0512d0b5524cf
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-07-19 11:33:31 +0300

    styles: Added animation on FAQ

--- components/sections/faq/FAQ.tsx
+++ components/sections/faq/FAQ.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 
 import { StarIcon } from "@/components/icons";
+import AnimatedList from "@/components/shared/animated/AnimatedList";
+import AnimatedListItem from "@/components/shared/animated/AnimatedListItem";
+import AnimatedWrapper from "@/components/shared/animated/AnimatedWrapper";
 import Marquee from "@/components/shared/marquee/Marquee";
+import { fadeInAnimation } from "@/helpers/animation";
 import { ROUTES } from "@/config/routes";
 
 import FaqList from "./FaqList";
@@ -12,8 +16,11 @@ const Faq = () => {
       id={ROUTES.FAQ.id}
       className="relative scroll-mt-20 overflow-hidden pb-[113px] pt-10 xl:pb-[197px] xl:pt-[130px]"
     >
-      <div className="container relative z-[1] max-w-[600px] xl:max-w-[1280px]">
-        <div className="relative mb-6 overflow-hidden rounded-full bg-accent px-5 py-6 xl:mb-10 xl:p-11">
+      <div className="container relative z-[10] max-w-[600px] xl:max-w-[1280px]">
+        <AnimatedWrapper
+          animation={fadeInAnimation({ x: -50 })}
+          className="relative mb-6 overflow-hidden rounded-full bg-accent px-5 py-6 xl:mb-10 xl:p-11"
+        >
           <h2 className="text-[18px] font-bold uppercase leading-[1.2] text-light xl:text-[48px]">
             Часті питання
           </h2>
@@ -23,18 +30,25 @@ const Faq = () => {
           <StarIcon className="absolute -bottom-[5px] right-[43px] size-[44px] text-accent-light xl:-bottom-5 xl:right-[88px] xl:size-[100px]" />
           <StarIcon className="absolute -top-[8px] right-[11px] size-[55px] text-accent-light xl:-top-[22px] xl:right-[14px] xl:size-[124px]" />
           <StarIcon className="absolute -right-[2px] bottom-[1px] size-[33px] text-accent-light xl:-bottom-[6px] xl:-right-[15px] xl:size-[73px]" />
-        </div>
+        </AnimatedWrapper>
 
-        <FaqList />
+        <AnimatedList>
+          <AnimatedListItem>
+            <FaqList />
+          </AnimatedListItem>
+        </AnimatedList>
       </div>
 
-      <Image
-        src="/images/faq/faq-decor-desk.svg"
-        alt="Декоративне зображення"
-        width={401}
-        height={580}
-        className="absolute bottom-[90px] right-0 object-cover max-xl:hidden xl:block"
-      />
+      {/* <AnimatedWrapper animation={fadeInAnimation({ x: 50, scale: 0.8 })}> */}
+      <div className="absolute right-0 top-[450px] !-z-[1] h-[580px] w-[401px] object-cover max-xl:hidden">
+        <Image
+          src="/images/faq/faq-decor-desk.svg"
+          alt="Декоративне зображення"
+          fill
+          sizes="401px"
+        />
+      </div>
+      {/* </AnimatedWrapper> */}
 
       <Marquee className="absolute bottom-0 left-0" variant="dark" />
     </section>
