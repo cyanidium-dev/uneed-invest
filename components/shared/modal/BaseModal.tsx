15830576c8872bd6565badb9a7bdfc79d1b9f5a9
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-08-18 11:06:58 +0300

    Update BaseModal.tsx

--- components/shared/modal/BaseModal.tsx
+++ components/shared/modal/BaseModal.tsx
@@ -61,7 +61,7 @@ const BaseModal = ({
     >
       <div
         className={cn(
-          "relative flex h-[347px] w-[304px] flex-col items-center rounded-xl bg-light md:w-[440px]",
+          "relative flex h-[347px] w-[304px] flex-col items-center rounded-lg bg-light md:w-[440px]",
           modalClassName
         )}
       >
